c4a6ec832829bc8ae807cc8e82455ca79db5f952
Einstein1987 <45828264+Einstein1987@users.noreply.github.com>
2024-07-18 11:34:08 +0200

    Create globals.js

---
+++ functions/globals.js
@@ -0,0 +1,8 @@
+export let questions = [];
+export let currentQuestion = 0;
+export let responses = [];
+export let recognition;
+export let synthesis = window.speechSynthesis;
+export let recordingStudentInfo = false;
+export let recordingResponse = false;
+export let speechRate = 1.0;
