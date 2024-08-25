d2f88ad4ba789581b26e6239ce15aac3f35031b6
Einstein1987 <45828264+Einstein1987@users.noreply.github.com>
2024-08-25 17:12:07 +0200

    Update fetch-doc.js

--- functions/fetch-doc.js
+++ functions/fetch-doc.js
@@ -1,1 +1,36 @@
-test
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  try {
+    const { url } = JSON.parse(event.body);
+
+    if (!url) {
+      return {
+        statusCode: 400,
+        body: 'URL manquante dans la requête.'
+      };
+    }
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      return { statusCode: response.status, body: 'Erreur lors de la récupération du document.' };
+    }
+    
+    // Traiter la réponse en tant que blob pour les PDF
+    const buffer = await response.arrayBuffer();
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': 'inline; filename="document.pdf"',
+      },
+      body: Buffer.from(buffer).toString('base64'),
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Erreur serveur: ' + error.message
+    };
+  }
+};
